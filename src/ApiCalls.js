dd3a9db410a4b793fcb3dbf4ba8f922e4a85d79c
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-10-02 21:16:11 -0400

    added axios instance

---
+++ src/ApiCalls.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001'
+
+const axiosInstance = axios.create({
+    baseURL: baseUrl,
+    timeout: 1000,
+    withCredentials: true,
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+})
+
+export default axiosInstance
