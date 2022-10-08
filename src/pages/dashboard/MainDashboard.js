2d1f8093de872bbce933b34d8e6330f27e9d9666
Kassim Ballout <54900156+kballout@users.noreply.github.com>
2022-10-08 14:30:05 -0400

    created initialization page

--- src/pages/dashboard/MainDashboard.js
+++ src/pages/dashboard/MainDashboard.js
@@ -1,36 +1,49 @@
-import React, {useEffect, useState} from 'react'
-import { useSelector } from 'react-redux'
-import { useLocation, useParams} from 'react-router-dom'
-import '../../styles/main.css'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import "../../styles/main.css";
 
-function MainDashboard({id}) {
-  
-  const {selectedGuild} = useSelector((state) => state.auth)
+function MainDashboard({ id }) {
+  const { selectedGuild } = useSelector((state) => state.auth);
 
-  
-
-  
   return (
     <div>
-        <div className='mainDash'>
-          <section>
-            {!selectedGuild ? (
-              <div>
-                <h1>
-                  Server Settings
-                </h1>
-                <p>
-                  Please choose a server from the left sidebar to change settings. You can also add GuildMaster to a new server with the plus button.
-                </p>
-              </div>
-            ): <div>
+      <div className="mainDash">
+        <section>
+          {!selectedGuild ? (
+            <div>
+              <h1>Server Settings</h1>
+              <p>
+                Please choose a server from the left sidebar to change settings.
+                You can also add GuildMaster to a new server with the plus
+                button.
+              </p>
+            </div>
+          ) : (
+            <div>
               <h1>Server Settings</h1>
               <h2>{selectedGuild.name}</h2>
-            </div>}
-          </section>
-        </div>
+              <div className="initialized">
+                {selectedGuild.exists === true ? (
+                  <div>
+                    <h5>Guild is initialized</h5>
+                    <p>The terminate button will end the activity and delete all channels, roles, and commands associated with GuildMaster</p>
+                    <Button>Terminate</Button>
+                  </div>
+                ) : (
+                  <div>
+                    <h5>Guild is not initialized</h5>
+                    <p>The Initialize button will begin the activity and create all channels, roles, and commands associated with GuildMaster</p>
+                    <Button>Initialize</Button>
+                  </div>
+                )}
+            </div>
+            </div>
+          )}
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MainDashboard
+export default MainDashboard;
